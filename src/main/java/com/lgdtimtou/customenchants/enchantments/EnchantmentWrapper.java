f2282a5cf69156e70bb32bbf8c449858b81c1142
ticoucke <coucketimon@gmail.com>
2022-06-03 10:08:48 +0200

    CustomEnchant v1.0 - Wrapper class included

---
+++ src/main/java/com/lgdtimtou/customenchants/enchantments/EnchantmentWrapper.java
@@ -0,0 +1,59 @@
+package com.lgdtimtou.customenchants.enchantments;
+
+import org.bukkit.NamespacedKey;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.enchantments.EnchantmentTarget;
+import org.bukkit.inventory.ItemStack;
+
+public class EnchantmentWrapper extends Enchantment {
+
+    private final String name;
+    private final int maxLvl;
+
+
+    public EnchantmentWrapper(String namespace, String name, int maxLvl) {
+        super(NamespacedKey.minecraft(namespace));
+        this.name = name;
+        this.maxLvl = maxLvl;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getMaxLevel() {
+        return maxLvl;
+    }
+
+    @Override
+    public int getStartLevel() {
+        return 1;
+    }
+
+    @Override
+    public EnchantmentTarget getItemTarget() {
+        return null;
+    }
+
+    @Override
+    public boolean isTreasure() {
+        return false;
+    }
+
+    @Override
+    public boolean isCursed() {
+        return false;
+    }
+
+    @Override
+    public boolean conflictsWith(Enchantment other) {
+        return false;
+    }
+
+    @Override
+    public boolean canEnchantItem(ItemStack item) {
+        return false;
+    }
+}
